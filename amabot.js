b08490b52d8fe8ee3b977e4d26eea334d613bcfc
Alex <ouatu.alexandru@gmail.com>
2021-04-23 22:28:24 +0200

    italian edition

--- amabot.js
+++ amabot.js
@@ -16,7 +16,68 @@ const retry = async (promiseFactory, retryCount) => {
   }
 }
 
-const runAmabot = async () => {
+const runAmaitbot = async () => {
+  const browser = await puppeteer.launch({
+    args: ['--disable-features=site-per-process'],
+    headless: false
+  })
+  const page = await browser.newPage()
+  await retry(() => {
+    return page.goto(
+      'https://amazon.it/gp/sign-in.html',
+      { waitUntil: 'domcontentloaded', timeout: 5000 }
+    )
+  }, 10)
+  await page.type('#ap_email', email)
+  await page.click('#continue')
+  await page.waitForNavigation()
+  await page.type('#ap_password', password)
+  await page.click('#signInSubmit')
+  await page.waitForNavigation()
+  await retry(() => {
+    return Promise.all([
+      page.goto(
+        `https://www.amazon.it/dp/${productId}`,
+        { waitUntil: 'domcontentloaded', timeout: 5000 }),
+      page.waitForSelector('#availability', { timeout: 5000 })
+    ])
+  }, 10)
+  let availabilityElement = await page.$('#availability')
+  let availabilityText = await page.evaluate(getElementTextContent, availabilityElement)
+  while (
+    !availabilityText.trim().toLowerCase().includes('disponibilità immediata.')
+    && !availabilityText.trim().toLowerCase().includes('disponibilità: solo ')
+    || availabilityText.trim().toLowerCase().includes('Non disponibile')
+    ) {
+    await sleep(10000)
+    await page.setCacheEnabled(false)
+    await retry(() => {
+      return Promise.all([
+        page.reload({ waitUntil: 'domcontentloaded', timeout: 15000 }),
+        page.waitForSelector('#availability', { timeout: 15000 })
+      ])
+    }, 10)
+    availabilityElement = await page.$("#availability")
+    availabilityText = await page.evaluate(getElementTextContent, availabilityElement)
+    console.log('Not available :(', new Date())
+  }
+  console.log('Available :D')
+  const priceElement = await page.$('#price_inside_buybox')
+  const priceText = await page.evaluate(getElementTextContent, priceElement)
+  const priceStrTrimmed = priceText.trim()
+  const len = priceStrTrimmed.length;
+  let price = priceStrTrimmed.substring(0,len-1)
+  price = price.replace(",", ".");
+  if (Number(price) <= priceYoureWillingToPay) {
+    await page.click('#buy-now-button')
+    await page.waitForSelector('#submitOrderButtonId-announce')
+    await page.click('#submitOrderButtonId input[name=placeYourOrder1]');
+  } else {
+    console.log('rip it was a scalp', price)
+  }
+}
+
+const runAmaCombot = async () => {
   const browser = await puppeteer.launch({
     args: ['--disable-features=site-per-process'],
     headless: false
@@ -78,4 +139,8 @@ const runAmabot = async () => {
   }
 }
 
-runAmabot()
+if (config.region === '.com' || !config.region) {
+  runAmaCombot()
+} else if(config.region === '.it') {
+  runAmaitbot();
+}
